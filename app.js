7d9cd16fd20787dcd8412e30b54fdf9699044589
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-04-09 04:39:09 +0700

    Added cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,10 +16,7 @@ const hash = require('./helpers/hash');
 
 let app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -26,8 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
