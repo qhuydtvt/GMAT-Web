6a480ea992d0e0ac652e0b20485f7c60e1ed1941
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-04-16 01:54:14 +0700

    Added cors setting for credentials

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({}));
+app.use(cors({ origin: [ 'http://localhost:3000', 'https://huynhtuanhuy.github.io'], credentials: true }));
 
 app.get('/', (req, res) => {
   res.send("Hello World");
