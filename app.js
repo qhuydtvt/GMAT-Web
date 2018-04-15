959637cfbbe11697852c0df32f699706b15ffcbc
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-04-16 00:24:05 +0700

    Try to fix herokuapp error

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const hbs = require('hbs');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 
 const settings = require('./settings');
@@ -22,6 +19,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({}));
 
 app.get('/', (req, res) => {
@@ -30,23 +28,21 @@ app.get('/', (req, res) => {
 
 app.use('/api', apiRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.send('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
 
 mongoose.connect(settings.dbUri, (err) => {
   if(err) console.log(err);
