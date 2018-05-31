0ada5221bb7a6f3aaebd4271be7b79bdf64f7035
qhuydtvt <qhuydtvt@gmail.com>
2018-05-31 18:45:35 +0700

    Added userchoices as array in test result

--- models/testResult.model.js
+++ models/testResult.model.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const UserChoiceSchema = new Schema({
+  choice: {type: Number, default: -1},
+  time: {type: Number, default: 0}
+});
+
 const AnswersSchema = new Schema({
     question: { type: Schema.Types.ObjectId, ref: 'Question' },
     choice: { type: Number, default: null },
-    time: { type: Number, default: 0 }
+    time: { type: Number, default: 0 },
+    userChoices: [ UserChoiceSchema ]
 }, {
     _id: false 
 });
