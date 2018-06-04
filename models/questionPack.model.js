ad59ac39f03717468500c95c33cb3a269c82efcf
qhuydtvt <qhuydtvt@gmail.com>
2018-06-04 16:20:10 +0700

    Added header to question pack

--- models/questionPack.model.js
+++ models/questionPack.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 let QuestionPackSchema = new Schema({
     name: { type: String},
+    header: {type: String},
     questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }]
 }, {
     timestamps: true
