e4225ae8d151864a817a58a0b1cc565ecb6656c7
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-08-03 11:09:39 +0700

    Update index.js
    Move db to local

--- settings/index.js
+++ settings/index.js
@@ -2,7 +2,7 @@ module.exports = {
     port: 6996,
     superSecret: '3dnTz4548Tyb4uWXpVucbyh3khuE42nDZS7QYDVZHh4xF9NrQLfXLA5MGJSAdUtR',
     masterPasswordHash: '$2a$10$UYE8uwEBJMz3KI4hJtPA7uFvCLlqlM.CXKrdTw.UbVhqdHFulTlAq',
-    dbUri: 'mongodb://admin:webfullstack@ds239309.mlab.com:39309/gmat-web',
+    dbUri: 'mongodb://localhost/gmat-web',
     permission: {
         auth: '1',
         classrooms: '2',
